1dbc65c1a2df01ebc1ea3451372d3dbd0b9a8218
king_sarp <sarpongnana9@gmail.com>
2025-06-08 17:18:39 +0000

    Done search Component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,11 @@
-import SearchForm from "../components/SearchForm";
+import SearchForm from "../../components/SearchForm";
 
-export default function Home() {
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query || "";
   return (
     <section className="pink_container">
       <h1 className="heading">
@@ -13,7 +18,7 @@ export default function Home() {
         innovation, and connect with like-minded individuals who can help bring
         your startup to life.
       </p>
-      <SearchForm />
+      <SearchForm query = {query} />
     </section>
   );
 }
